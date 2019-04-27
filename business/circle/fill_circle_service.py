8e21540473a1cf82adc201097fc3f917db00b4a0
AlbertGandolf <AlbertGandolf@outlook.com>
2019-04-27 12:42:31 +0800

    add_serial

--- business/circle/fill_circle_service.py
+++ business/circle/fill_circle_service.py
@@ -3,30 +3,29 @@
 from rust.core import business
 
 from business.account.account import Account
+from business.circle.join_in_service import JoinInService
 
 from db.circle import models as circle_models
 from db.account import models as account_models
+
 
 class FillCircleService(business.Service):
 
 
 	def __fill_member_data(self, circles):
-		id2circle = {circle.id: circle for circle in circles}
 
 		circle_ids = [circle.id for circle in circles]
 		record_db_models = circle_models.CircleMember.select().dj_where(circle_id__in=circle_ids)
 		account_ids = [record_db_model.account_id for record_db_model in record_db_models]
 		db_models = account_models.Account.select().dj_where(id__in=account_ids)
-
+		account_id2account = {db_model.id: Account(db_model) for db_model in db_models}
 		for circle in circles:
 			circle.members = []
+			member_ids = JoinInService(self.user).get_serial_member_ids(circle.id)
+			for member_id in member_ids:
+				circle.members.append(account_id2account[member_id])
 
-		for db_model in db_models:
-			id2circle.setdefault(db_model.circle_id, []).members.append(Account(db_model))
-
-		for circle in circles:
 			circle.member_amount = len(circle.members)
-
 
 	def fill(self, circles, options=None):
 		options = options or {}
--- business/circle/join_in_service.py
+++ business/circle/join_in_service.py
@@ -5,6 +5,8 @@ from rust.core.exceptions import BusinessError
 
 from db.circle import models as circle_models
 
+CIRCLE_ID2MEMBERS = {}
+
 
 class JoinInService(business.Service):
 	"""
@@ -14,6 +16,7 @@ class JoinInService(business.Service):
 		"""
 		加入圈子
 		"""
+		global CIRCLE_ID2MEMBERS
 		if circle_models.CircleMember.select().dj_where(
 				account_id=param_object.account_id,
 				circle_id=param_object.circle_id
@@ -24,12 +27,26 @@ class JoinInService(business.Service):
 				account_id=param_object.account_id,
 				circle_id=param_object.circle_id
 			)
+			CIRCLE_ID2MEMBERS.setdefault(param_object.circle_id, default=[]).append(param_object.account_id)
 
 	def exit(self, param_object):
 		"""
 		退出圈子
 		"""
+		global CIRCLE_ID2MEMBERS
 		circle_models.CircleMember.delete().dj_where(
 			account_id=param_object.account_id,
 			circle_id=param_object.circle_id
 		).execute()
+		if CIRCLE_ID2MEMBERS.setdefault(param_object.circle_id, default=[]).count(param_object.account_id) > 0:
+			CIRCLE_ID2MEMBERS[param_object.circle_id].remove(param_object.account_id)
+
+	def get_serial_member_ids(self, param_object):
+		"""
+		获取按照加入顺序排列的用户id列表
+		"""
+		global CIRCLE_ID2MEMBERS
+		circle_id = param_object.circle_id
+
+		return CIRCLE_ID2MEMBERS.setdefault(circle_id, default=[])
+
