05583a6b347ea14b93be579a76746867149632a7
AlbertGandolf <AlbertGandolf@outlook.com>
2019-04-27 13:49:47 +0800

    simple_run

--- business/circle/fill_circle_service.py
+++ business/circle/fill_circle_service.py
@@ -14,17 +14,21 @@ class FillCircleService(business.Service):
 		id2circle = {circle.id: circle for circle in circles}
 
 		circle_ids = [circle.id for circle in circles]
-		record_db_models = circle_models.CircleMember.select().dj_where(circle_id__in=circle_ids)
+		record_db_models = circle_models.CircleMember.select().dj_where(circle_id__in=circle_ids).order_by('id')
+
+		join_order = {}
+		for record_db_model in record_db_models:
+			join_order.setdefault(record_db_model.circle_id, []).append(record_db_model.account_id)
+
 		account_ids = [record_db_model.account_id for record_db_model in record_db_models]
 		db_models = account_models.Account.select().dj_where(id__in=account_ids)
-
+		account_id2account = {db_model.id: Account(db_model) for db_model in db_models}
 		for circle in circles:
 			circle.members = []
+			serial_account_ids = join_order.setdefault(circle.id, [])
+			for serial_account_id in serial_account_ids:
+				circle.members.append(account_id2account[serial_account_id])
 
-		for db_model in db_models:
-			id2circle.setdefault(db_model.circle_id, []).members.append(Account(db_model))
-
-		for circle in circles:
 			circle.member_amount = len(circle.members)
 
 
